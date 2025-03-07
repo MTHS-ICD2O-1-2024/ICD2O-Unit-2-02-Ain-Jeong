7205e85fa4545c81109e3dbc6f19a1d28f6d66da
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-03-07 20:17:37 +0000

    fixed js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,6 @@
 // This file contains the JS functions for index.html
 
 function doMathClicked() {
-  document.getElementById ("area-math").innerHTML = "<p>The area is: " + (5 * 3) + " cm²</p>"
-  document.getElementById ("perimeter-math").innerHTML = "<p>The perimeter is: " + (2 * (5 + 3)) + " cm</p>"
+  document.getElementById ("area-math").innerHTML = "<p>The area is: " + 5 * 3 + " cm²</p>"
+  document.getElementById ("perimeter-math").innerHTML = "<p>The perimeter is: " + 2 * (5 + 3) + " cm</p>"
 }
